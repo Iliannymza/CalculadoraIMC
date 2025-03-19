d760fe00794a5484e34e810299697c2fd50de81f
Iliannymza <mendoza71997@gmail.com>
2025-03-19 19:48:13 +0100

    working in a new design 0.1 sin terminar

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -2,15 +2,12 @@ package com.example.calculadoraimc
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.slider.Slider
-import org.w3c.dom.Text
-import kotlin.math.log
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
@@ -33,21 +30,29 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
+
+        // busco los componentes en la vista y los guardo en sus respectivas variables
+        //weigth
         removeWeightButton = findViewById(R.id.removeWeightButton )
         addWeightButton = findViewById(R.id.addWeightButton)
         weightTextView = findViewById(R.id.weightTextView)
+        //heigth
         heightSlider = findViewById(R.id.heightSlider)
         heightTextView = findViewById(R.id.heightTextView)
+        //result
         calculateButton = findViewById(R.id.calculateButton)
         resultTextView = findViewById(R.id.resultTextView)
+        descriptionTextView = findViewById(R.id.descriptionTextView)
 
 
         removeWeightButton.setOnClickListener {
@@ -68,6 +73,35 @@ class MainActivity : AppCompatActivity() {
             val result = weigth / (heigth / 100).pow(2)
 
             resultTextView.text = String.format("%.2f" , result)
+
+            var colorId = 0
+            var descriptionId = ""
+            when (result) {
+                in 0f ..<18.5f -> {
+                colorId = getColor(R.color.bmi_underweight)
+                descriptionId = getString(R.string.bmi_underweight)
+            }
+                in 18.5f..< 25f -> {colorId = getColor(R.color.bmi_normalweight)
+                descriptionId = getString(bmi_underweight)
+            }
+                in 25f..<30f -> {colorId = getColor(R.color.bmi_overweight)
+                descriptionId = getString(bmi_overweight)
+
+            }
+                in 30f..<35f -> {
+                colorId = getColor(R.color.obecity)
+                descriptionId = getString(obecity)
+
+            }
+                else -> {
+                    colorId = getColor(R.color.extreme_obecity)
+                    descriptionId = getString(extreme_obecity)
+            }
+
+            }
+            descriptionTextView.text = getString(descriptionId)
+            descriptionTextView.setTextColor(getColor(colorId))
+            resultTextView.setTextColor(colorId)
         }
     }
 
